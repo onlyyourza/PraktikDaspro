6cf1822e974d83e592353804f6e1ac3b8d9929f7
onlyyourza <paseoking456@gmail.com>
2025-10-09 13:42:24 +0700

    Task 1

--- jobsheet5/ifCetakKRS14.java
+++ jobsheet5/ifCetakKRS14.java
@@ -3,18 +3,27 @@ import java.util.Scanner;
 
 public class ifCetakKRS14 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.println("---Print KRS SIAKAD---");
-        System.out.println("Have the tuition fees been paid in full? (true/false): ");
-        boolean uktLunas = sc.nextBoolean();
+        System.out.print("Have the tuition fees been paid in full? (true/false): ");
+        boolean uktLunas = input.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("UKT Payment has been verified");
-            System.out.println("KRS now can be printed and you can ask the academic advisor to sign it.");
-        } else {
-            System.out.println("Registration rejected. Please pay ukt first.");
-        }
-        sc.close();
+        // Bentuk IF-ELSE
+        // if (uktLunas) {
+        //     System.out.println("UKT Payment has been verified");
+        //     System.out.println("KRS now can be printed and you can ask the academic advisor to sign it.");
+        // } else {
+        //     System.out.println("Registration rejected. Please pay UKT first.");
+        // }
+
+        // Bentuk Ternary Operator
+        String message = uktLunas 
+            ? "UKT Payment has been verified. KRS now can be printed and you can ask the academic advisor to sign it." 
+            : "Registration rejected. Please pay UKT first.";
+            
+        System.out.println(message);
+
+        input.close();
     }
 }
